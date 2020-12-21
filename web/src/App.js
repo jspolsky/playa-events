5c96a42d7d4f48291024c6b2a859437ac084251a
Joel Spolsky <spolsky@fogcreek.com>
2020-12-21 16:41:22 -0500

    move and resize events

--- web/src/App.js
+++ web/src/App.js
@@ -1,29 +1,50 @@
 import React, { useState } from "react";
 import { Header } from "./components/Header.js";
+
 import { Calendar, Views, momentLocalizer } from "react-big-calendar";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
 import moment from "moment";
 import { burningManDates, yearDefault } from "./dateFunctions.js";
 import { BurnWeek } from "./components/BurnWeek.js";
 import { events as initialEvents } from "./sampleEvents";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.scss";
 
 // Playa Events!
 
 const localizer = momentLocalizer(moment);
+const DraggableCalendar = withDragAndDrop(Calendar);
+
+// Give each event a unique identity so we can identify them
+// later in drag and drops
+var identity = 0;
+const eventsWithIDs = initialEvents.map((x) => {
+  return { ...x, id: identity++ };
+});
+
 function App() {
-  const [events, setEvents] = useState(initialEvents);
+  const [events, setEvents] = useState(eventsWithIDs);
   const [firstDay, lastDay] = burningManDates(yearDefault());
 
   const handleSelect = ({ start, end }) => {
     const title = window.prompt("New Event name");
     if (title) {
-      setEvents([...events, { start, end, title }]);
+      setEvents([...events, { start, end, title, id: identity++ }]);
     }
+  };
+
+  const moveEvent = ({ event, start, end, isAllDay }) => {
+    setEvents(
+      events.map((i) => {
+        return i.id === event.id ? { ...i, start, end, allDay: !!isAllDay } : i;
+      })
+    );
   };
 
   return (
     <div className="App">
       <Header />
-      <Calendar
+      <DraggableCalendar
         localizer={localizer}
         defaultDate={firstDay}
         defaultView={Views.WEEK}
@@ -34,6 +55,8 @@ function App() {
         onSelectEvent={(event) => alert(event.title)}
         onSelectSlot={handleSelect}
         showMultiDayTimes
+        onEventDrop={moveEvent}
+        onEventResize={moveEvent}
       />
     </div>
   );
