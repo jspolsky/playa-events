0d0258576663c17cfa948b8fc689ab1ae0c2e21a
Joel Spolsky <spolsky@fogcreek.com>
2021-01-08 10:57:34 -0500

    refactoring

--- web/src/components/EventDialog.js
+++ web/src/components/EventDialog.js
@@ -53,65 +53,135 @@ const WeeButton = ({ name, button, onClick }) => {
   );
 };
 
-const EventDialogHeader = ({ editing, setEditing, event, close, title }) => {
-  return editing ? (
-    <Modal.Header>
-      <Modal.Title>{title}</Modal.Title>
-      <div style={{ marginLeft: "auto" }}>
-        <WeeButton
-          name="Close"
-          button={closeButton}
-          onClick={() => {
-            close();
-          }}
-        />
-      </div>
-    </Modal.Header>
-  ) : (
-    <Modal.Header>
-      <Modal.Title>{title}</Modal.Title>
-      <div style={{ marginLeft: "auto" }}>
-        <WeeButton
-          name="Edit"
-          button={editButton}
-          onClick={() => {
-            setEditing(true);
-          }}
-        />
-        <WeeButton name="Delete" button={deleteButton} />
-        <WeeButton name="Close" button={closeButton} onClick={close} />
-      </div>
-    </Modal.Header>
+const EventDialogWhen = ({ days, start, end }) => {
+  return (
+    <div>
+      {days.map((d) => (
+        <>
+          {moment(firstDay).add(d, "days").format("ddd, MMM D")}
+          <br />
+        </>
+      ))}
+      <strong>
+        {moment({ hour: start.h, minute: start.m }).format("h:mma")}
+        &mdash;
+        {moment({ hour: end.h, minute: end.m }).format("h:mma")}
+      </strong>
+      <em>{end.h < start.h ? " next day" : ""}</em>
+    </div>
   );
 };
 
-const EventDialogWhen = ({ editing, event }) => {
-  return editing ? (
-    <Table size="sm">
-      <tbody>
-        <tr>
-          {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"].map(
-            (x, i) => (
-              <td className="text-center" key={`dow${i}`}>
-                {x}
-              </td>
-            )
-          )}
-        </tr>
-        <tr>
-          {[...Array(9)].map((x, i) => (
-            <td className="text-center" key={`dow${i}`}>
-              <Form.Check
-                inline
-                style={{ marginRight: 0 }}
-                type="checkbox"
-              ></Form.Check>
-            </td>
-          ))}
-        </tr>
-      </tbody>
+const EventDialogStatic = ({
+  days,
+  start,
+  end,
+  description,
+  title,
+  close,
+  setEditing,
+}) => {
+  return (
+    <>
+      <Modal.Header>
+        <Modal.Title>{title}</Modal.Title>
+        <div style={{ marginLeft: "auto" }}>
+          <WeeButton
+            name="Edit"
+            button={editButton}
+            onClick={() => {
+              setEditing(true);
+            }}
+          />
+          <WeeButton name="Delete" button={deleteButton} />
+          <WeeButton name="Close" button={closeButton} onClick={close} />
+        </div>
+      </Modal.Header>
 
-      {/* <Form.Row>
+      <Modal.Body>
+        <EventDialogWhen days={days} start={start} end={end} />
+        <br></br>
+        {description}
+        <br></br>
+        <br></br>
+        <strong>Location:</strong> Christmas Camp
+        <br />
+        <i>(camp address will be provided by Placement)</i>
+        <br />
+        <br />
+        <span style={{ fontSize: "2.2rem" }}>🤸‍♀️ 🍽 🔞</span>
+      </Modal.Body>
+    </>
+  );
+};
+
+const EventDialogEditing = ({
+  title,
+  setTitle,
+  description,
+  setDescription,
+  close,
+}) => {
+  return (
+    <>
+      <Modal.Header>
+        <Modal.Title>{title}</Modal.Title>
+        <div style={{ marginLeft: "auto" }}>
+          <WeeButton
+            name="Close"
+            button={closeButton}
+            onClick={() => {
+              close();
+            }}
+          />
+        </div>
+      </Modal.Header>
+      <Modal.Body>
+        <Form.Group>
+          <Form.Label>Event name</Form.Label>
+          <Form.Control
+            size="lg"
+            type="text"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            placeholder="Event name"
+          />
+          <br />
+          <Table size="sm">
+            <tbody>
+              <tr>
+                {[
+                  "Sun",
+                  "Mon",
+                  "Tue",
+                  "Wed",
+                  "Thu",
+                  "Fri",
+                  "Sat",
+                  "Sun",
+                  "Mon",
+                ].map((x, i) => (
+                  <td className="text-center" key={`dow${i}`}>
+                    {x}
+                  </td>
+                ))}
+              </tr>
+              <tr>
+                {[...Array(9)].map((x, i) => (
+                  <td className="text-center" key={`dow${i}`}>
+                    <Form.Check
+                      inline
+                      style={{ marginRight: 0 }}
+                      type="checkbox"
+                    ></Form.Check>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+
+            {/* <Form.Row>
           <Col>
             <Form.Label>Start Time</Form.Label>
             <Form.Control
@@ -133,93 +203,35 @@ const EventDialogWhen = ({ editing, event }) => {
             ></Form.Control>
           </Col>
         </Form.Row> */}
-    </Table>
-  ) : (
-    <div>
-      {event.days.map((d) => (
-        <>
-          {moment(firstDay).add(d, "days").format("ddd, MMM D")}
+          </Table>
+
           <br />
-        </>
-      ))}
-      <strong>
-        {moment({ hour: event.start.h, minute: event.start.m }).format("h:mma")}
-        &mdash;
-        {moment({ hour: event.end.h, minute: event.end.m }).format("h:mma")}
-      </strong>
-      <em>{event.end.h < event.start.h ? " next day" : ""}</em>
-    </div>
+
+          <br />
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={5}
+            placeholder="Description"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </Form.Group>
+      </Modal.Body>
+      <Modal.Footer>Save</Modal.Footer>
+    </>
   );
-};
-
-const EventDialogBody = ({
-  editing,
-  event,
-  title,
-  setTitle,
-  description,
-  setDescription,
-}) => {
-  return editing ? (
-    <Modal.Body>
-      <Form.Group>
-        <Form.Label>Event name</Form.Label>
-        <Form.Control
-          size="lg"
-          type="text"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-          placeholder="Event name"
-        />
-        <br />
-        <EventDialogWhen editing={editing} event={event} />
-
-        <br />
-
-        <br />
-        <Form.Label>Description</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={5}
-          placeholder="Description"
-          value={description}
-          onChange={(e) => {
-            setDescription(e.target.value);
-          }}
-        />
-      </Form.Group>
-    </Modal.Body>
-  ) : (
-    <Modal.Body>
-      <EventDialogWhen editing={editing} event={event} />
-      <br></br>
-      {description}
-      <br></br>
-      <br></br>
-      <strong>Location:</strong> Christmas Camp
-      <br />
-      <i>(camp address will be provided by Placement)</i>
-      <br />
-      <br />
-      <span style={{ fontSize: "2.2rem" }}>🤸‍♀️ 🍽 🔞</span>
-    </Modal.Body>
-  );
-};
-
-const EventDialogFooter = ({ editing }) => {
-  if (editing) {
-    return <Modal.Footer>Save</Modal.Footer>;
-  } else {
-    return <div />;
-  }
 };
 
 export const EventDialog = ({ show, close, event }) => {
   const [editing, setEditing] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [days, setDays] = useState([]);
+  const [start, setStart] = useState({ h: 12, m: 0 });
+  const [end, setEnd] = useState({ h: 12, m: 0 });
 
   useEffect(() => {
     if (show) {
@@ -227,6 +239,9 @@ export const EventDialog = ({ show, close, event }) => {
       // pull out editable data from event
       setTitle(event.title);
       setDescription(event.description);
+      setDays(event.days);
+      setStart(event.start);
+      setEnd(event.end);
     } else {
       // dialog just disappeared
       setEditing(false);
@@ -246,22 +261,25 @@ export const EventDialog = ({ show, close, event }) => {
       size="lg"
       centered
     >
-      <EventDialogHeader
-        event={event}
-        title={title}
-        close={handleClose}
-        editing={editing}
-        setEditing={setEditing}
-      />
-      <EventDialogBody
-        event={event}
-        title={title}
-        setTitle={setTitle}
-        description={description}
-        setDescription={setDescription}
-        editing={editing}
-      />
-      <EventDialogFooter editing={editing} />
+      {editing ? (
+        <EventDialogEditing
+          title={title}
+          setTitle={setTitle}
+          description={description}
+          setDescription={setDescription}
+          close={handleClose}
+        />
+      ) : (
+        <EventDialogStatic
+          days={days}
+          start={start}
+          end={end}
+          description={description}
+          title={title}
+          close={handleClose}
+          setEditing={setEditing}
+        />
+      )}
     </Modal>
   );
 };
