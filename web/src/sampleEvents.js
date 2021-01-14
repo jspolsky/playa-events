2244d9460672f4c06d0e17fea4b061f2ef103e0f
Joel Spolsky <spolsky@fogcreek.com>
2021-01-14 11:22:21 -0500

    better events

--- web/src/sampleEvents.js
+++ web/src/sampleEvents.js
@@ -15,7 +15,7 @@ export const initialRawEvents = [
     days: [1],
     title: "East Village Shirtless Dance Party",
     description:
-      "Future Turtles invites you to our opening night bash. Cosmos, dance music, and hot bartenders",
+      "Future Turtles invites you to our opening night bash. Cosmos, dance music, hugs and friendly bartenders.",
     atCamp: true,
     type: "prty",
   },
@@ -43,22 +43,55 @@ export const initialRawEvents = [
     start: { h: 11, m: 0 },
     end: { h: 12, m: 0 },
     days: [2, 3, 4, 5, 6],
-    title: "HIIT Workout",
+    title: "Lazy workout",
     description:
-      "Join the turtles for High Intensity Interval Training. Good music, smoothies. Bring yoga mat.",
+      "Join the turtles for High Intensity Interval Training, only we're super lazy so it won't be that high intensity. Good music, smoothies. Bring yoga mat.",
     atCamp: true,
     type: "care",
   },
   {
     start: { h: 18, m: 0 },
     end: { h: 23, m: 59 },
-    days: [2, 3, 4, 5],
+    days: [2, 4],
     title: "Your Dreams Interpreted",
     description:
       "Visit the Future Turtles straight-friendly bar to ogle our hot bartenders, and get your dreams interpreted. Unique alcoholic and non-alcoholic cocktails every night.",
     atCamp: true,
     type: "prty",
   },
+  {
+    start: { h: 18, m: 0 },
+    end: { h: 23, m: 59 },
+    days: [3],
+    title: "Cook yer sausage",
+    description:
+      "Visit the Future Turtles straight-friendly bar to ogle our hot bartenders, and cook your sausage in our deep boiling pots of fragrant water. BRING YOUR OWN SAUSAGE. (If you have a cow, you can make sausage at Farm Camp, Esplanade and 6:15). Unique alcoholic and non-alcoholic cocktails every night.",
+    atCamp: true,
+    type: "food",
+  },
+  {
+    start: { h: 16, m: 0 },
+    end: { h: 18, m: 0 },
+    days: [4],
+    title: "Billion Bunny March",
+    description:
+      "Come over to the bunny side. Hop. Wiggle your tail. Deliver colorful eggs and support a universal campaign of fluffy goodness for all. NO CARROTS",
+    atCamp: false,
+    location:
+      "Meet at Center Camp; march will proceed to the Man at 4:30 sharp",
+    type: "para",
+  },
+  {
+    start: { h: 18, m: 0 },
+    end: { h: 23, m: 59 },
+    days: [5],
+    title: "AP Calculus Study Group",
+    description:
+      "Trying to get a leg up before school resumes next week? Our expert instructors will cover the most important parts of Leibniz's amazing invention. Unique alcoholic and non-alcoholic cocktails will be served.",
+    atCamp: true,
+    type: "work",
+  },
+
   {
     start: { h: 0, m: 45 },
     end: { h: 2, m: 0 },
