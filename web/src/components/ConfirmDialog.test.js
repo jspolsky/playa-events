b338e8462dc45d090ddc39805e2456fcc3347fef
Joel Spolsky <spolsky@fogcreek.com>
2021-01-16 16:06:51 -0500

    first ever jest test of a component

---
+++ web/src/components/ConfirmDialog.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import { ConfirmDialog } from "./ConfirmDialog.js";
+
+test("Confirm Dialog", () => {
+  const fn = () => {
+    return null;
+  };
+
+  const tree = render(
+    <ConfirmDialog
+      open={true}
+      setOpen={fn}
+      title="Sample title here"
+      message="Some message appears"
+      no="Cancel"
+      yes="Yes, Delete it"
+      doIt={fn}
+    />
+  );
+
+  expect(tree).toMatchSnapshot();
+});
